9f3ab48f24096c33df91caca823a649b1b0bcb41
Mussa Ali Haider <musaalihaider5@gmail.com>
2024-09-06 20:21:03 +0500

    next app

--- src/app/order-product/page.tsx
+++ src/app/order-product/page.tsx
@@ -1,5 +1,4 @@
 "use client"
-import React from "react";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 export default function order(){
@@ -12,9 +11,10 @@ export default function order(){
         <div className="backgw">
             <h1 >Order your product</h1>
             <br />
-            <p>Obviously this is just an assignment so im using a hook on placing order by pressing it you wil go the home page. </p>
+            <p>Obviously this is just an assignment so im using a hook on placing order by pressing it you wil go the home page </p>
             <br />
             <Button onClick={handleClick}>place order</Button>
+            <br />
         </div>
     )
 }
